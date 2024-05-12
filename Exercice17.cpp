a896fab2309553801b92d4bbecd2ce5586e7d955
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-05-12 20:40:31 +0200

    Create Exercice17.cpp

---
+++ Exercice17.cpp
@@ -0,0 +1,47 @@
+/*
+Écrire un programme qui calcule les racines carrées de nombres fournis en
+donnée. Il s’arrêtera lorsqu’on lui fournira la valeur 0. Il refusera les valeurs
+négatives. Son exécution se présentera ainsi :
+donnez un nombre positif : 2
+sa racine carrée est : 1.414214e+00
+donnez un nombre positif : -1
+svp positif
+donnez un nombre positif : 5
+sa racine carrée est : 2.236068e+00
+donnez un nombre positif : 0
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std ;
+int main(){
+    double (nombre);
+    while(true) {
+        cout << "Donnez un nombre positif : ";
+        cin >> nombre;
+        
+        if (nombre < 0){
+            cout << "svp positif" << endl;
+        }
+        else if (nombre==0) {
+            cout << "Fin du programme";
+            return 0;
+        }
+        else {
+            cout << "Sa racine carrée est : " << scientific << sqrt(nombre) << endl;
+        }
+    }
+}
+
+/* Résultats
+Donnez un nombre positif : -5
+svp positif
+Donnez un nombre positif : 81
+Sa racine carrée est : 9.000000e+00
+Donnez un nombre positif : 100
+Sa racine carrée est : 1.000000e+01
+Donnez un nombre positif : 2
+Sa racine carrée est : 1.414214e+00
+Donnez un nombre positif : 0
+Fin du programme
+*/
